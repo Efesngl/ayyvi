5f270dd3d53c1cf4e54c8fdcbacfd050ec384bf6
Efe Şengül <86547599+Efesngl@users.noreply.github.com>
2024-05-21 18:54:34 +0300

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,7 +10,7 @@ import "dropzone/dist/dropzone.css"
 window.bootstrap=bootstrap
 createInertiaApp({
   resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: false })
     return pages[`./Pages/${name}.vue`]
   },
   setup({ el, App, props, plugin }) {
